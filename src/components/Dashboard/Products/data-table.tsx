d53bf4bc5f0b355ff1fbb6ad25f5f4607f5d003b
Sajjad177 <sajjadhossainx06@gmail.com>
2026-02-20 00:56:40 +0600

    loading and pagination added

--- src/components/Dashboard/Products/data-table.tsx
+++ src/components/Dashboard/Products/data-table.tsx
@@ -33,7 +33,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className="rounded-md border bg-white shadow-sm">
+    <div className="rounded-md border bg-white">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
